ed0a6a069ca9b4b02ecdc61d1e3d05ed754c3d30
Adam Powell <developeradam@imac.home>
2021-08-09 20:52:28 +0100

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='grid items-center grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10 px-20 sm:px-32 py-14 bg-gray-100 text-gray-600'>
+      <div className='space-y-3 text-gray-800 text-xs items-center flex flex-col '>
+        <h5 className='font-bold'>ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p> Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className='space-y-3 text-gray-800 text-xs items-center flex flex-col '>
+        <h5 className='font-bold'>HOST</h5>
+        <p>How Airbnb works</p>
+        <p> Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+
+      <div className='space-y-3 text-gray-800 text-xs items-center flex flex-col '>
+        <h5 className='font-bold'>SUPPORT</h5>
+        <p>How Airbnb works</p>
+        <p> Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className='space-y-3 text-gray-800 text-xs items-center flex flex-col '>
+        <h5 className='font-bold'>PIZZA</h5>
+        <p>How Airbnb works</p>
+        <p> Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
